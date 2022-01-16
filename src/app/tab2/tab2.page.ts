61b3f8af901dddbe9de4d97b175c6b0771cccf41
Juan David Valencia Toro <juanda.vid1996@hotmail.com>
2022-01-15 20:43:58 -0500

    test ok

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,92 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  // variables
+  nombre: string;
+  tip_documento: string;
+  num_documento: number;
+  telefono: number;
+  direccion: string;
+  nombre_producto: string;
+  descripcion_producto: string;
+  factura: string;
+  extra1: string;
+  extra2: string;
+  extra3: string;
+  subtotal: string;
+  precio_neto: number;
+  iva: number;
+  total: number;
+
+  // funcion que capturan los inputs
+  datos_pagos = {
+    nombre: '',
+    tip_documento:'cc',
+    num_documento: '',
+    telefono: '',
+    direccion: '',
+    nombre_producto: '',
+    descripcion_producto: '',
+    factura: '',
+    precio_neto: '',
+    iva: '',
+    extra1: '',
+    extra2: '',
+    extra3: '',
+    total: '',
+  };
+
   constructor() {}
+
+  ngOnInit(){}
+
+
+  //llaves epayco
+  window: any = window;
+  handler = this.window.ePayco.checkout.configure({
+    key: "d42ae82ca25bd9b0f3877a574183c4d7", //public_key d42ae82ca25bd9b0f3877a574183c4d7
+    test: false
+  });
+
+
+  epayco(){
+    console.log('Form submit');
+    console.log( this.datos_pagos );
+
+    var data = {
+        //Parametros compra (obligatorio)
+        name: this.datos_pagos.nombre_producto,
+        description: this.datos_pagos.descripcion_producto,
+        invoice: this.datos_pagos.factura,
+        currency: "cop",
+        amount: this.datos_pagos.precio_neto+this.datos_pagos.iva,
+        tax_base: this.datos_pagos.precio_neto,
+        tax: this.datos_pagos.iva,
+        country: "co",
+        lang: "en",
+        //Onpage="false" - Standard="true"
+        external: "false",
+
+        //Atributos opcionales
+        extra1: this.datos_pagos.extra1,
+        extra2: this.datos_pagos.extra2,
+        extra3: this.datos_pagos.extra3,
+        confirmation: "http://localhost/test-david/confirmation.php",
+        response: "http://localhost/test-david/response.php",
+
+        //Atributos cliente
+        name_billing: this.datos_pagos.nombre,
+        address_billing: this.datos_pagos.direccion,
+        type_doc_billing: this.datos_pagos.tip_documento,
+        mobilephone_billing: this.datos_pagos.telefono,
+        number_doc_billing: this.datos_pagos.num_documento,
+
+        //atributo deshabilitación metodo de pago
+        //methodsDisable: ["TDC", "PSE","SP","CASH","DP"]
+        methodsDisable: [], //habilito los otros metodos de pago
+    };
+    this.handler.open(data);
+    console.log( data );
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as epayco from 'epayco-sdk-node';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,91 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  //Varibles 
+  nombres: string;
+  apellidos: string;
+  tipodedocumento: string[]=["CC","CE","PPN", "LIC", "NIT", "TI", "DNI"];
+  numerodedocumento: number;
+  correoelectronico: string;
+  direccion: string;
+  ciudad: string;
+  telefono: number;
+  celular: number;
+  valor_a_pagar: number;
+  nombretitulartarjeta: string;
+  numerotarjetatc: number;
+  mes_expiracion: number;
+  ano_expiracion: number;
+  cvc_tc: number;
+  cuotas: number;
+
+  //Capturar imputs formulario
+  datos_pagos_tc = {
+    nombres: '',
+    apellidos: '',
+    tipodedocumento:'CC',
+    numerodedocumento: '',
+    correoelectronico: '',
+    direccion: '',
+    ciudad: '',
+    telefono: '',
+    celular: '',
+    valor_a_pagar: '',
+    nombretitulartarjeta: '',
+    numerotarjetatc: '',
+    mes_expiracion: '',
+    ano_expiracion: '',
+    cvc_tc: '',
+    cuotas: '',
+  }
+
   constructor() {}
+
+  ngOnInit(){ }
+
+  
+
+  EpaycoSdk(){
+    console.log('Form submit');
+    console.log( this.datos_pagos_tc);
+
+    var require: any;
+
+    var epayco = require("epayco-node-master")({
+      apiKey: "d42ae82ca25bd9b0f3877a574183c4d7", //public_key d42ae82ca25bd9b0f3877a574183c4d7
+      privateKey: "7eba53ab76dbb2c39a7435a79d27b6f8", //private_key 7eba53ab76dbb2c39a7435a79d27b6f8
+      lang: "ES",
+      test: true,
+      });
+
+      var credit_info = {
+        "card[number]": this.datos_pagos_tc.numerotarjetatc,
+        "card[exp_year]": this.datos_pagos_tc.ano_expiracion,
+        "card[exp_month]": this.datos_pagos_tc.mes_expiracion,
+        "card[cvc]": this.datos_pagos_tc.cvc_tc
+        };
+
+        epayco.token.create(credit_info).then(function (token) {
+          console.log(token);
+  
+          // var customer_info = {
+          //     token_card: token.id,
+          //     name: this.datos_pagos_tc.nombres,
+          //     last_name: this.datos_pagos_tc.apellidos,
+          //     email: this.datos_pagos_tc.correoelectronico, 
+          //     default: true,
+          //     city: this.datos_pagos_tc.ciudad,
+          //     address: this.datos_pagos_tc.direccion,
+          //     phone: this.datos_pagos_tc.telefono,
+          //     cell_phone: this.datos_pagos_tc.celular,
+          // };
+  
+      }).catch(function (err) {
+          console.log("err: " + err);
+      });
+
+
+
+  }
 
 }
